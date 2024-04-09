c7d1d9f4ff4d7a7718153875579598bc54ae2ac3
Alec Reynolds <alec+git@lando.dev>
2024-04-08 20:30:02 -0700

    Improve contribution information.

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -49,8 +49,8 @@ function sidebar() {
         {text: 'GitHub', link: 'https://github.com/lando/python/issues/new/choose'},
         {text: 'Slack', link: 'https://www.launchpass.com/devwithlando'},
         {text: 'Contact Us', link: '/support'},
+        {text: 'Examples', link: 'https://github.com/lando/python/tree/main/examples'},
       ],
     },
-    {text: 'Examples', link: 'https://github.com/lando/python/tree/main/examples'},
   ];
 };
